7b59c2aa91b8f87a912603dd846e0d0f0b4058c8
xiaoheiwa <47276687+xiaoheiwa@users.noreply.github.com>
2023-05-25 20:17:29 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,4 @@
-// 这里编写自定义js脚本；将被静态引入到页面中
+// 鼠标点击烟花特效
+FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || false, // 开关
+// 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
+FIREWORKS_COLOR: ['255, 20, 97', '24, 255, 146', '90, 135, 255', '251, 243, 140'],
